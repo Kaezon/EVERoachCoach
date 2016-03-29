410074f34678e5bf7c8cda6135a82354e9c66e6a
Brett Costabile <kaezontpa@gmail.com>
2016-03-28 21:49:21 -0400

    Fixed some mistakes

--- portal/views.py
+++ portal/views.py
@@ -2,4 +2,4 @@ from django.conf import settings
 from django.shortcuts import render
 
 def dashboard(request):
-    return render(request, {'service_name': settings.SERVICE_NAME}, 'public/dashboard.html')
+    return render(request, 'public/dashboard.html', {'service_name': settings.SERVICE_NAME})
